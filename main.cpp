0fd74dccefba64b14c1861257deee0c795e0c61e
Yuta Suzuki <banquet.of.merry.widow.y.s@gmail.com>
2016-06-06 01:00:23 +0900

    load image as gray scale check image type and channels

--- main.cpp
+++ main.cpp
@@ -33,12 +33,14 @@ int main(int argc, char** argv)
 	cout << "input file:" << src << endl;
 	
 
-	cv::Mat image = cv::imread(src);
+	cv::Mat image = cv::imread(src, CV_LOAD_IMAGE_GRAYSCALE);
+	CV_Assert(image.channels() == 1 && image.type() == CV_8UC1);
 	showImage(image);
 
 	cv::Mat binary;
 	int binaryMax = 255;
 	int binaryThreshold = 128;
 	cv::threshold(image, binary, binaryThreshold, binaryMax, cv::THRESH_BINARY_INV);
+	CV_Assert(binary.channels() == 1 && image.type() == CV_8UC1);
 	showImage(binary);
 }
