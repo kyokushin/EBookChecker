a446e53d474b9df336f7dcd9bebb3a7fa0f8b314
Yuta Suzuki <banquet.of.merry.widow.y.s@gmail.com>
2016-06-06 22:58:55 +0900

    二値化時の最大値を1に。 積分画像を見やすくする処理を追加。 コメントも追加。 デバッグ時のマクロが間違っていたので修正

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+//画像表示用関数
 int showImage(const cv::Mat& image, int wait = 0){
 	static const string wname("EBookChecker");
 
@@ -20,6 +21,7 @@ int main(int argc, char** argv)
 
 
 #ifndef _DEBUG
+	//コマンドライン引数の解析。デバッグの時は使わない
 	string commandArgs =
 		"@input |  | processing one image or image named serial number"
 		;
@@ -27,27 +29,43 @@ int main(int argc, char** argv)
 	cv::CommandLineParser parser(argc, argv, commandArgs);
 
 	string src = parser.get<string>(0);
-#endif
+#else
 	string src = TEST_DATA_0;
+#endif
 
 	cout << "input file:" << src << endl;
 	
 
+	//画像の読み込み。グレースケール画像として読み込む
 	cv::Mat image = cv::imread(src, CV_LOAD_IMAGE_GRAYSCALE);
 	CV_Assert(image.channels() == 1 && image.type() == CV_8UC1);
 	showImage(image);
 
+	//二値化
 	cv::Mat binary;
-	int binaryMax = 255;
+	int binaryMax = 1;//二値化時の最大値は1に。積分するときに白だったところか黒だったところかがわかればいい。
 	int binaryThreshold = 128;
 	cv::threshold(image, binary, binaryThreshold, binaryMax, cv::THRESH_BINARY_INV);
 	CV_Assert(binary.channels() == 1 && image.type() == CV_8UC1);
 	showImage(binary);
 	cv::imwrite("binary.jpg", binary);
 
+	//積分画像の生成
 	cv::Mat integral;
 	cv::integral(binary, integral);
 	CV_Assert(integral.channels() == 1 && integral.type() == CV_32SC1);
 	showImage(integral);
 	cv::imwrite("integral.jpg", integral);
+
+	//積分画像を見やすくする処理
+	cv::Mat integralVisible;
+	double max;
+	double min;
+	cv::minMaxLoc(integral, &min, &max);//最大値だけほしいので第3引数まで。最小値は0のはずだが本当に0か確認するために使う
+	CV_Assert(min == 0.0);//本当に最小値が0になっているか確認
+
+	integral.convertTo(integralVisible, CV_8UC1, 255.0 / max); //betaは使わない。0-255の値をとるようにalphaを与える。
+	showImage(integralVisible);
+	cv::imwrite("integralVisible.jpg", integralVisible);
+
 }
